c18577e83a0d0dc5eecfc3de697d3792e2aa29a0
Apoorv Upreti <apoorvupreti@gmail.com>
2015-12-12 19:21:29 +0000

    when-we-were-high-first-commit

--- alarm.js
+++ alarm.js
@@ -1,62 +1,67 @@
 // NOTE: Use python -m SimpleHTTPServer on the directory with the track.
-// TODO: Put this project under source control
-// Make the XMLHttpRequest call async in weather.js
 function playAudio(file_path) {
-	var audio = new Audio();
-	audio.src = file_path;
-	audio.play();
+    var audio = new Audio();
+    audio.src = file_path;
+    audio.play();
 }
 
 function playAlarm(alarm_name) {
-	var file_path = getAlarmTrack(alarm_name);
-	playAudio(file_path);
+    var file_path = getAlarmTrack(alarm_name);
+    playAudio(file_path);
 }
 
 function getAlarmTimeInMsFromEpoch(time_of_day) {
-	var parts = time_of_day.split(":");
-	var alarm_hours = parseInt(parts[0]);
-	var alarm_minutes = parseInt(parts[1]);
-	var current_date = new Date(); 
-	var current_hours = current_date.getHours();
-	var current_minutes = current_date.getMinutes();
-	var offset = 0;
-	var alarm_date = new Date(current_date);
-	alarm_date.setHours(alarm_hours);
-	alarm_date.setMinutes(alarm_minutes);
-	alarm_date.setSeconds(0);
-	if (current_date.getTime() > alarm_date.getTime()) {
-		offset = 24 * 60 * 60 * 1000;
-		alarm_date = new Date(alarm_date.getTime() + offset);
-	}
-	return Date.now() + 2000;
-	return alarm_date.getTime();
+    var parts = time_of_day.split(":");
+    var alarm_hours = parseInt(parts[0]);
+    var alarm_minutes = parseInt(parts[1]);
+    var current_date = new Date(); 
+    var current_hours = current_date.getHours();
+    var current_minutes = current_date.getMinutes();
+    var offset = 0;
+    var alarm_date = new Date(current_date);
+    alarm_date.setHours(alarm_hours);
+    alarm_date.setMinutes(alarm_minutes);
+    alarm_date.setSeconds(0);
+    if (current_date.getTime() > alarm_date.getTime()) {
+        offset = 24 * 60 * 60 * 1000;
+        alarm_date = new Date(alarm_date.getTime() + offset);
+    }
+    return Date.now() + 2000;
+    return alarm_date.getTime();
 }
 
 function setAlarm(alarm_name) {
-	chrome.alarms.create(alarm_name, {
-		"when": getAlarmTimeInMsFromEpoch(getAlarmTime(alarm_name)),
-		"periodInMinutes": 24 * 60,
-	});
+    if (typeof chrome.alarms !== 'undefined') {
+        chrome.alarms.create(alarm_name, {
+            "when": getAlarmTimeInMsFromEpoch(getAlarmTime(alarm_name)),
+            "periodInMinutes": 24 * 60,
+        });
+    } else {
+        console.log('chrome.alarms is undefined. Trying again.');
+    }
 }
 
 function speakAlarmMessage(alarm_name) {
-	var alarm_message = getAlarmMessage(alarm_name);
-	var utterance = new SpeechSynthesisUtterance(alarm_message);
-	utterance.voice = speechSynthesis.getVoices()[1];
-	utterance.lang = "en-US";
-	utterance.rate = 0.8;
-	speechSynthesis.speak(utterance);
+    getAlarmMessage(alarm_name, function(alarm_message) {
+        var utterance = new SpeechSynthesisUtterance(alarm_message);
+        utterance.voice = speechSynthesis.getVoices()[1];
+        utterance.lang = "en-US";
+        utterance.rate = 0.8;
+        console.log("SAYING: " + alarm_message);
+        speechSynthesis.speak(utterance);
+    });
 }
 
-
-getAlarmList().forEach(function(alarm_name) {
-	setAlarm(alarm_name);
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (!isValidAlarm(alarm.name)) {
+        return;
+    }
+    //playAlarm(alarm.name);
+    speakAlarmMessage(alarm.name);
 });
 
-chrome.alarms.onAlarm.addListener(function (alarm) {
-	if (!isValidAlarm(alarm.name)) {
-		return;
-	}
-	//playAlarm(alarm.name);
-	speakAlarmMessage(alarm.name);
-})
+chrome.app.runtime.onLaunched.addListener(function() {
+    getAlarmList().forEach(function(alarm_name) {
+        setAlarm(alarm_name);
+    });
+});
--- config.js
+++ config.js
@@ -1,42 +1,40 @@
 
 var alarm_config = {
-	"wake_up": {
-		"time": "06:00",
-		"play": "http://localhost:8000/wakeup.mp3",
-		"weather_location": {
-			"latitude": 51.5265822,
-			"longitude": -0.1235648
-		},
-		"message": alarmMessageGenerator
-	}
+    "wake_up": {
+        "time": "06:00",
+        "play": "http://localhost:5000/static/wakeup.mp3",
+        "weather_location": {
+            "latitude": 51.5265822,
+            "longitude": -0.1235648
+        },
+        "message": alarmMessageGenerator
+    }
 }
 
 function isValidAlarm(alarm_name) {
-	if (alarm_name in alarm_config) {
-		return true;
-	}
-	return false;
+    if (alarm_name in alarm_config) {
+        return true;
+    }
+    return false;
 }
 
 function getAlarmTime(alarm_name) {
-	console.log(alarm_config);
-	console.log(alarm_name);
-	return alarm_config[alarm_name]["time"];
+    return alarm_config[alarm_name]["time"];
 }
 
 function getAlarmTrack(alarm_name) {
-	return alarm_config[alarm_name]["play"];
+    return alarm_config[alarm_name]["play"];
 }
 
-function getAlarmMessage(alarm_name) {
-	var weather_location = getWeatherLocation(alarm_name);
-	return alarm_config[alarm_name]["message"](weather_location);
+function getAlarmMessage(alarm_name, callback) {
+    var weather_location = getWeatherLocation(alarm_name);
+    alarm_config[alarm_name]["message"](weather_location, callback);
 }
 
 function getWeatherLocation(alarm_name) {
-	return alarm_config[alarm_name]["weather_location"];
+    return alarm_config[alarm_name]["weather_location"];
 }
 
 function getAlarmList() {
-	return Object.keys(alarm_config);
+    return Object.keys(alarm_config);
 }
